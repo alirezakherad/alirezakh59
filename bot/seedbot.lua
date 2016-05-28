75283660fa8c642a7c0bec489bdf4805d533efd6
alirezakherad <a.kheradmandnezhad@gmail.com>
2016-05-28 05:38:16 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {alirezakh59},--Sudo users
+    sudo_users = {alirezakh59polis},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
